3cd2ac0e96b64832232c4b5f441c79cee97404a1
23f2001890 <23f2001890@ds.study.iitm.ac.in>
2026-07-15 17:00:26 +0530

    First Commit

---
+++ proxy_server_with_cache.c
@@ -0,0 +1,109 @@
+#include "proxy_parse.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define MAX_CLIENTS 10
+
+typedef struct cache_element cache_element;
+
+
+
+// cache linked list Node
+struct cache_element{
+    char* data;
+    int len;
+    char* url;
+    time_t lru_time_track;
+    cache_element* element;
+};
+
+cache_element* find(char* url);
+int add_cache_element(char* data, int size , char* url);
+void remove_cache_element();
+
+int port_number = 8080;
+int proxy_socketId;
+pthread_t tid[MAX_CLIENTS];
+sem_t semaphore;
+pthread_mutex_t lock;
+
+cache_element* head;
+int cache_size;
+
+int main(int argc,char* argv[]){
+    int client_socketId, client_len;
+    struct sockaddr_in server_addr,client_addr;
+    sem_init(&semaphore,0,MAX_CLIENTS);
+    pthread_mutex_init(&lock,NULL);
+    
+    if(argv == 2){
+        port_number = atoi(argv[1]);
+        // ./proxy 8090
+    }else{
+        printf("Too few arguments\n");
+        exit(1);
+    }
+
+    printf("Starting Proxy server at port: %d\n",port_number);
+    proxy_socketId = socket(AF_INET, SOCK_STREAM,0);
+    // af_inet-> tcp,upd  ; sockstream -> 2 way communication
+    if(proxy_socketId<0){
+        perror("Failed to create a scoket\n");
+        exit(1);
+    }
+    int reuse = 1;
+    if(setsockopt(proxy_socketId, SOL_SOCKET,SO_REUSEADDR,(const char*)& reuse,sizeof(reuse))<0){
+        perror("setSockOpt failed\n");
+    }
+
+    bzero((char*)& server_addr,sizeof(server_addr)); //makes everychar 0;
+    server_addr.sin_family = AF_INET; //ipv4 tcp
+    server_addr.sin_port = htons(port_number); // to make code understable by network
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    if(bind(proxy_socketId,(struct sockaddr*)& server_addr,sizeof(server_addr)<0)){
+        perror("Port not available\n");
+        exit(1);
+    } // binding server to port;
+    printf("Binding on port %d\n",port_number);
+    int listen_status = listen(proxy_socketId,MAX_CLIENTS); //proxy server global port listening
+    if(listen_status<0){
+        perror("Error in listening\n");
+        exit(1);
+    }
+    int i=0;
+    int Connected_socketId[MAX_CLIENTS];
+
+    while(1){
+        bzero((char *)& client_addr, sizeof(client_addr));
+        client_len = sizeof(client_addr);
+        client_socketId = accept(proxy_socketId,(struct sockaddr*)& client_addr, (socklen_t*)&client_len);
+        if(client_socketId<0){
+            printf("Not able to connect");
+            exit(1);
+        }
+        else{
+            Connected_socketId[i]= client_socketId;
+        }
+
+        struct sockaddr_in * client_pt = (struct sockaddr_in *)& client_addr;
+        struct in_addr ip_addr = client_pt->sin_addr;
+        char str[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET,&ip_addr,str,INET_ADDRSTRLEN);
+        
+    }
+
+
+}
